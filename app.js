291a67cd00e8c2bd2617515109dbdd6c2f7f4813
brandon <webarebrandy@gmail.com>
2024-01-02 16:05:46 -0600

    app.js endpoint alumnos

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // app.js
-
 const express = require('express');
+const bodyParser = require('body-parser');
+const Alumno = require('./models/alumnoModel');
+const pool = require('./models/conexion');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+app.use(bodyParser.json());
+
+// Ruta para registrar un nuevo alumno
+app.post('/alumnos', async (req, res) => {
+  try {
+    const resultado = await Alumno.registrar(req.body);
+    res.status(201).json(resultado);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al registrar el alumno' });
+  }
 });
 
 app.listen(port, () => {
