7a9b1677839345dbee8db77085a855da1c0f261c
brandon <webarebrandy@gmail.com>
2024-01-02 16:25:29 -0600

    manejo de menzajes

--- app.js
+++ app.js
@@ -11,14 +11,16 @@ app.use(bodyParser.json());
 
 // Ruta para registrar un nuevo alumno
 app.post('/alumnos', async (req, res) => {
-  try {
-    const resultado = await Alumno.registrar(req.body);
-    res.status(201).json(resultado);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error al registrar el alumno' });
-  }
-});
+    try {
+      const resultado = await Alumno.registrar(req.body);
+      console.log('Alumno registrado correctamente en la base de datos:', resultado);
+  
+      res.status(201).json({ mensaje: 'Alumno registrado correctamente' });
+    } catch (error) {
+      console.error('Error al registrar el alumno:', error);
+      res.status(500).json({ error: 'Error al registrar el alumno' });
+    }
+  });
 
 app.listen(port, () => {
   console.log(`La aplicación está escuchando en http://localhost:${port}`);
