371bf54428959fa6ff0d01af98cdf7740a70b114
brandon <webarebrandy@gmail.com>
2024-01-02 16:49:19 -0600

    servidor solo alumnos en nodemon

--- app.js
+++ app.js
@@ -9,33 +9,6 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-// Ruta para registrar un nuevo alumno
-app.post('/alumnos', async (req, res) => {
-    try {
-      const resultado = await Alumno.registrar(req.body);
-      console.log('Alumno registrado correctamente en la base de datos:', resultado);
-  
-      res.status(201).json({ mensaje: 'Alumno registrado correctamente' });
-    } catch (error) {
-      console.error('Error al registrar el alumno:', error);
-      res.status(500).json({ error: 'Error al registrar el alumno' });
-    }
-  });
-
-app.listen(port, () => {
-  console.log(`La aplicación está escuchando en http://localhost:${port}`);
-});
-// app.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const Alumno = require('./models/alumnoModel');
-const pool = require('./models/conexion');
-
-const app = express();
-const port = 3000;
-
-app.use(bodyParser.json());
-
 // Conectar a la base de datos
 pool.getConnection((err, connection) => {
   if (err) {
