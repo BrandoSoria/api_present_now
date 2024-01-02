ce6ce42380bb8335ea3714c7098cab0c5b34cd5b
brandon <webarebrandy@gmail.com>
2024-01-02 16:42:18 -0600

    inicio de servidor con nodemon

--- app.js
+++ app.js
@@ -25,3 +25,53 @@ app.post('/alumnos', async (req, res) => {
 app.listen(port, () => {
   console.log(`La aplicación está escuchando en http://localhost:${port}`);
 });
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const Alumno = require('./models/alumnoModel');
+const pool = require('./models/conexion');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// Conectar a la base de datos
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err.message);
+  } else {
+    console.log('Conexión a la base de datos establecida correctamente');
+    connection.release();
+  }
+});
+
+// Ruta para registrar un nuevo alumno
+app.post('/alumnos', async (req, res) => {
+  try {
+    const resultado = await Alumno.registrar(req.body);
+    console.log('Alumno registrado correctamente en la base de datos:', resultado);
+
+    res.status(201).json({ mensaje: 'Alumno registrado correctamente' });
+  } catch (error) {
+    console.error('Error al registrar el alumno:', error);
+    res.status(500).json({ error: 'Error al registrar el alumno' });
+  }
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  // Configuración para reiniciar automáticamente en modo de desarrollo
+  const server = app.listen(port, () => {
+    console.log(`La aplicación está escuchando en http://localhost:${port}`);
+  });
+
+  const io = require('socket.io')(server);
+  io.on('connection', (socket) => {
+    console.log('¡Un cliente se ha conectado!');
+  });
+} else {
+  // Configuración para producción
+  app.listen(port, () => {
+    console.log(`La aplicación está escuchando en http://localhost:${port}`);
+  });
+}
--- models/alumnoModel.js
+++ models/alumnoModel.js
@@ -3,8 +3,17 @@ const pool = require('./conexion');
 
 const Alumno = {
   registrar: async (alumno) => {
-    const [rows, fields] = await pool.execute('INSERT INTO alumnos (numero_de_control, nombre_completo, grupo, carrera, semestre, materia, horario) VALUES (?, ?, ?, ?, ?, ?, ?)',
+    // Verificar si ya existe un alumno con el mismo número de control
+    const [existente] = await pool.execute('SELECT * FROM alumnos WHERE numero_de_control = ?', [alumno.numero_de_control]);
+
+    if (existente.length > 0) {
+      throw new Error('Ya existe un alumno con este número de control');
+    }
+
+    // Si no existe, proceder con la inserción
+    const [rows] = await pool.execute('INSERT INTO alumnos (numero_de_control, nombre_completo, grupo, carrera, semestre, materia, horario) VALUES (?, ?, ?, ?, ?, ?, ?)',
       [alumno.numero_de_control, alumno.nombre_completo, alumno.grupo, alumno.carrera, alumno.semestre, alumno.materia, alumno.horario]);
+    
     return rows;
   }
 };
