0d82a010d70ee19cc6566d1f1bb1f84867ff60d0
brandon <webarebrandy@gmail.com>
2024-01-15 21:11:40 -0600

    cambio de variables

--- models/conexion.js
+++ models/conexion.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'dbpn',
+  host: process.env.MYSQLHOST || 'localhost',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '',
+  database: process.env.MYSQLDATABASE || 'dbpn',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
