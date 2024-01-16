4403541276c50c750042fbcb151b61bb7d1f0413
brandon <webarebrandy@gmail.com>
2024-01-15 20:43:59 -0600

    cambioen la conexion

--- models/conexion.js
+++ models/conexion.js
@@ -11,4 +11,17 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
+// Manejar errores de conexión
+pool.getConnection()
+  .then(connection => {
+    console.log('Conexión exitosa a la base de datos.');
+
+    // Realiza tus operaciones con la base de datos aquí.
+
+    connection.release(); // Devuelve la conexión a la pool cuando hayas terminado.
+  })
+  .catch(error => {
+    console.error('Error de conexión:', error.message);
+  });
+
 module.exports = pool;
